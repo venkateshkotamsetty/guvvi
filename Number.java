751e5ae9a0d394ff959f28350859e4f87e07458c
venkateshkotamsetty <41735214+venkateshkotamsetty@users.noreply.github.com>
2018-07-26 16:03:13 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner (System.in);
+		int a = s.nextInt();
+		   if (a > 0)
+		    {
+		    	System.out.println("positive");
+		    }
+		   else if (a == 0)
+		    {
+		    	System.out.println("zero");
+		    }
+		    	else
+		    	 {
+		    	 	System.out.println("nagative");
+		    	 }
+	}
+}
